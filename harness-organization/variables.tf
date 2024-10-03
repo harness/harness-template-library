c930dc669704b56e8562165c0d86c1b8f965b068
Jeremy Goodrum <jeremy.goodrum@harness.io>
2024-10-03 15:52:33 +0000

    FIX: HarnessOrganization to include harness_platform_url variable in declaration (#7)
    * FIX: HarnessOrganization to include harness_platform_url variable in declaration

--- harness-organization/variables.tf
+++ harness-organization/variables.tf
@@ -1,3 +1,9 @@
+variable "harness_platform_url" {
+  type        = string
+  description = "[Optional] Enter the Harness Platform URL.  Defaults to Harness SaaS URL"
+  default     = "https://app.harness.io/gateway"
+}
+
 variable "harness_platform_account" {
   type        = string
   description = "[Required] Enter the Harness Platform Account Number"
